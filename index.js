1a9d2013b3457fa1fe8d38b8763920cd799dac03
Subhajit Mondal <subhajit25mondal@gmail.com>
2018-11-19 01:06:10 +0530

    yo

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
+
+app.get('/api/users', function(req, res) {
+    var qu = req.param('q');
+
+     
+    var gsearch = require('gsearch');
+    gsearch.suggest(qu, function(error, data, res) {
+      res.send(JSON.stringify(data));
+    });
+
+
+  
+    //res.send(qu);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
